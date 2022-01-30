f7d6a1980be3cac22c093dc8783fa46d5dad4e24
Ivan Giuliani <giuliani.v@gmail.com>
2022-01-30 18:10:03 +0000

    Setup board rx serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
+#if !defined(SERIAL_SPEED)
+#  define SERIAL_SPEED 9600
+#endif
+
+void print_startup_header() {
+  Serial.printf("versions: core=%s boot=%u\n", ESP.getCoreVersion().c_str(), ESP.getBootVersion());
+  Serial.printf("boot mode: %u\n", ESP.getBootMode());
+  Serial.printf("cpu freq: %uMHz\n", ESP.getCpuFreqMHz());
+  Serial.printf("reset reason: %s\n", ESP.getResetReason().c_str());
+
+  Serial.println(F("System start OK."));
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(SERIAL_SPEED);
+  Serial.setTimeout(2000);
+  while(!Serial) { }
+
+  print_startup_header();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(100);
 }
